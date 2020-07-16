50594743af33b7109b4556526cdea50115b82496
Ahmad Mardiana <amdtkj@gmail.com>
2020-07-17 00:08:35 +0700

    fix connection

--- routes/connection.js
+++ routes/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connectionDb = mysql.createConnection({
   host:'localhost',
   user:'noc_alpha',
-  password:'P@55w0rd312',
+  password:'hellonoc',
   database:'noc_system'
 });
 module.exports = connectionDb;
