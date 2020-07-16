145fe5e938bda0dda953e079b1eea0c8606f5cef
Ahmad Mardiana <amdtkj@gmail.com>
2020-07-17 00:03:54 +0700

    fix connection to database

--- routes/connection.js
+++ routes/connection.js
@@ -2,8 +2,8 @@ var express = require('express');
 var mysql = require('mysql');
 var connectionDb = mysql.createConnection({
   host:'localhost',
-  user:'root',
-  password:'',
+  user:'noc_alpha',
+  password:'P@55w0rd312',
   database:'noc_system'
 });
 module.exports = connectionDb;
